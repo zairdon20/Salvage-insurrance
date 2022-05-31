f210e4d234c4c43a50c0a00020ac488d3dc00c47
zairdon20 <josphat2020@gmail.com>
2022-05-31 10:45:47 -0500

    Add the email form

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -5,8 +5,51 @@ class Signup extends React.Component {
     return (
       <div className="splash">
         Sign up
+        <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="userEmail">Email</label>
+        <input
+          type="text"
+          className="form-control"
+          id="userEmail"
+          name="username" />
       </div>
-    );
+      <div className="form-group">
+        <label htmlFor="username">First Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          name="username" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="username">Last Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          name="username" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Create Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="Password" />
+          </div>
+           <div className="form-group">
+        <label htmlFor="password">Confirm Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="Password" />
+      </div>
+      <button type="submit" className="btn btn-primary"></button>
+    </form>
+  </div>
+  </div>);
   }
 }
 
