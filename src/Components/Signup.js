60c62e89c4b84eed0b7b3319a9d2475b92574a62
zairdon20 <josphat2020@gmail.com>
2022-06-01 04:18:11 -0500

    Signup inputes

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,56 +1,20 @@
 import React from "react";
 
-class Signup extends React.Component {
-  render() {
-    return (
-      <div className="splash">
-        Sign up
-        <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="userEmail">Email</label>
-        <input
-          type="text"
-          className="form-control"
-          id="userEmail"
-          name="username" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="username">First Name</label>
-        <input
-          type="text"
-          className="form-control"
-          id="username"
-          name="username" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="username">Last Name</label>
-        <input
-          type="text"
-          className="form-control"
-          id="username"
-          name="username" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Create Password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          name="Password" />
-          </div>
-           <div className="form-group">
-        <label htmlFor="password">Confirm Password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          name="Password" />
-      </div>
-      <button type="submit" className="btn btn-primary"></button>
-    </form>
-  </div>
-  </div>);
-  }
-}
 
-export default Signup;
+export class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            confirmPassword: "",
+            error: ""
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+}
+export default Signup
